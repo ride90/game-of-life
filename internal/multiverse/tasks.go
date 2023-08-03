6590e1efd8f4b006fc52c2955c9f2c09f460716c
Oleh <oleh.pshenychnyi@showmax.com>
2023-08-03 21:57:21 +0200

    Coding in the mountains... Evolving algorithm.

--- internal/multiverse/tasks.go
+++ internal/multiverse/tasks.go
@@ -7,7 +7,7 @@ import (
 func EvolveMultiverseTask() {
 	// TODO: There should be some kind of lock to see if evolve is busy or not.
 	mv := GetInstance()
-	ticker := time.NewTicker(5 * time.Second)
+	ticker := time.NewTicker(300 * time.Millisecond)
 	for _ = range ticker.C {
 		mv.evolve()
 	}
--- internal/universe/universe.go
+++ internal/universe/universe.go
@@ -51,4 +51,74 @@ func (r *Universe) RenderMatrix() string {
 
 func (r *Universe) Evolve() {
 	fmt.Println("Evolving universe", r)
+	nextGenMatrix := r.Matrix
+
+	r.aliveCellsCount = 0
+	for y := range r.Matrix {
+		for x := range r.Matrix[y] {
+			neighborsCount := r.neighboursCount(x, y)
+			if r.Matrix[y][x] {
+				if neighborsCount < 2 || neighborsCount > 3 {
+					nextGenMatrix[y][x] = false
+				}
+			} else if neighborsCount == 3 {
+				nextGenMatrix[y][x] = true
+			}
+			if nextGenMatrix[y][x] {
+				r.aliveCellsCount++
+			}
+		}
+	}
+	r.Matrix = nextGenMatrix
+	r.generationNumber++
+}
+
+// neighboursCount method calculates the number of live neighbors for a given cell.
+func (r *Universe) neighboursCount(x, y int) int {
+	var ex, wx, ny, sy, neighbours int
+	if x != len(r.Matrix[y])-1 {
+		ex = x + 1
+	} else {
+		ex = 0
+	}
+	if x != 0 {
+		wx = x - 1
+	} else {
+		wx = len(r.Matrix[y]) - 1
+	}
+	if y != 0 {
+		ny = y - 1
+	} else {
+		ny = len(r.Matrix) - 1
+	}
+	if y != len(r.Matrix)-1 {
+		sy = y + 1
+	} else {
+		sy = 0
+	}
+	if r.Matrix[ny][wx] {
+		neighbours++
+	}
+	if r.Matrix[ny][x] {
+		neighbours++
+	}
+	if r.Matrix[ny][ex] {
+		neighbours++
+	}
+	if r.Matrix[y][wx] {
+		neighbours++
+	}
+	if r.Matrix[y][ex] {
+		neighbours++
+	}
+	if r.Matrix[sy][wx] {
+		neighbours++
+	}
+	if r.Matrix[sy][ex] {
+		neighbours++
+	}
+	if r.Matrix[sy][x] {
+		neighbours++
+	}
+	return neighbours
 }
