26969262c1dd5be5661a862c11e2cc32d4d7dcb2
Oleh <oleh.pshenychnyi@showmax.com>
2023-08-19 21:19:30 +0200

    Improve request logging.

--- middlewares/log_request.go
+++ middlewares/log_request.go
@@ -9,6 +9,7 @@ func MiddlewareLogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			log.Trace(r)
+			log.Infoln(r.Method, r.RemoteAddr, r.URL)
 			// Call the next handler, which can be another middleware in the chain, or the final handler.
 			next.ServeHTTP(w, r)
 		},
