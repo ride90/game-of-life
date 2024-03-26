52a2ffa729b6b9f630d406a49d08b29df41c840c
ride90 <olegpshenichniy@gmail.com>
2024-03-26 12:24:29 +0100

    Reformat settings

--- configs/config.go
+++ configs/config.go
@@ -10,25 +10,25 @@ import (
 // Config Struct to hold configuration values
 type Config struct {
 	Server struct {
-		Debug              bool   `yaml:"debug", envconfig:"SERVER_DEBUG"`
-		Host               string `yaml:"host", envconfig:"SERVER_HOST"`
-		Port               int    `yaml:"port", envconfig:"SERVER_PORT"`
-		WriteTimeout       int    `yaml:"write_timeout", envconfig:"SERVER_WRITE_TIMEOUT"`
-		ReadTimeout        int    `yaml:"read_timeout", envconfig:"SERVER_READ_TIMEOUT"`
-		WsWriteBufferSize  int    `yaml:"ws_write_buffer_size", envconfig:"SERVER_WS_WRITE_BUFFER_SIZE"`
-		WsReadBufferSize   int    `yaml:"ws_read_buffer_sie", envconfig:"SERVER_WS_READ_BUFFER_SIZE"`
-		WsHandshakeTimeout int    `yaml:"ws_handshake_timeout", envconfig:"SERVER_WS_HANDSHAKE_TIMEOUT"`
+		Debug              bool   `yaml:"debug" envconfig:"SERVER_DEBUG"`
+		Host               string `yaml:"host" envconfig:"SERVER_HOST"`
+		Port               int    `yaml:"port" envconfig:"SERVER_PORT"`
+		WriteTimeout       int    `yaml:"write_timeout" envconfig:"SERVER_WRITE_TIMEOUT"`
+		ReadTimeout        int    `yaml:"read_timeout" envconfig:"SERVER_READ_TIMEOUT"`
+		WsWriteBufferSize  int    `yaml:"ws_write_buffer_size" envconfig:"SERVER_WS_WRITE_BUFFER_SIZE"`
+		WsReadBufferSize   int    `yaml:"ws_read_buffer_sie" envconfig:"SERVER_WS_READ_BUFFER_SIZE"`
+		WsHandshakeTimeout int    `yaml:"ws_handshake_timeout" envconfig:"SERVER_WS_HANDSHAKE_TIMEOUT"`
 	} `yaml:"server"`
 
 	Game struct {
-		Fps                       int  `yaml:"fps", envconfig:"GAME_FPS"`
-		UniversePrepend           bool `yaml:"universe_prepend", envconfig:"GAME_UNIVERSE_PREPEND"`
-		RemoveStaticUniverseAfter int  `yaml:"remove_static_universe_after", envconfig:"GAME_REMOVE_STATIC_UNIVERSE_AFTER"`
+		Fps                       int  `yaml:"fps" envconfig:"GAME_FPS"`
+		UniversePrepend           bool `yaml:"universe_prepend" envconfig:"GAME_UNIVERSE_PREPEND"`
+		RemoveStaticUniverseAfter int  `yaml:"remove_static_universe_after" envconfig:"GAME_REMOVE_STATIC_UNIVERSE_AFTER"`
 	} `yaml:"game"`
 
 	Log struct {
-		Level           string `yaml:"level", envconfig:"LOG_LEVEL"`
-		SetReportCaller bool   `yaml:"set_report_caller", envconfig:"LOG_SET_REPORT_CALLER"`
+		Level           string `yaml:"level" envconfig:"LOG_LEVEL"`
+		SetReportCaller bool   `yaml:"set_report_caller" envconfig:"LOG_SET_REPORT_CALLER"`
 	} `yaml:"log"`
 }
 
